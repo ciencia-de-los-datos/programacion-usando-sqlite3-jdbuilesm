268d3dae65279da1bd0692d6e409871470813717
jdbuilesm <114514412+jdbuilesm@users.noreply.github.com>
2023-03-04 16:19:09 -0500

    Update pregunta_01.sql

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -17,6 +17,66 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
+#
+# Apertura de la conexión
+#
+import sqlite3
+
+conn = sqlite3.connect(":memory:")  ## aca se indica el nombre de la db.
+cur = conn.cursor()
+
+import pandas as pd
+
+tbl0 = pd.read_csv(
+    "tbl0.csv",
+    sep=",",
+    thousands=None,
+    decimal=".",
+    encoding="latin-1",
+    names=['K0', 'c01', 'c02', 'c03', 'c04']
+)
+
+tbl0.columns = [col.replace('-', '_') for col in tbl0.columns]
+
+tbl0.to_sql(
+    name="tbl0",
+    con=conn,
+    if_exists="replace",  # {‘fail’, ‘replace’, ‘append’}
+)
+tbl1 = pd.read_csv(
+    "tbl1.csv",
+    sep=",",
+    thousands=None,
+    decimal=".",
+    encoding="latin-1",
+    names=['K0', 'K1', 'c12', 'c13', 'c14', 'c15', 'c16']
+)
+
+tbl1.columns = [col.replace('-', '_') for col in tbl1.columns]
+
+tbl1.to_sql(
+    name="tbl1",
+    con=conn,
+    if_exists="replace",  # {‘fail’, ‘replace’, ‘append’}
+)
+
+tbl2 = pd.read_csv(
+    "tbl2.csv",
+    sep=",",
+    thousands=None,
+    decimal=".",
+    encoding="latin-1",
+    names=['K1', 'c21', 'c22', 'c23', 'c24', 'c25']
+)
+
+tbl2.columns = [col.replace('-', '_') for col in tbl2.columns]
+
+tbl2.to_sql(
+    name="tbl2",
+    con=conn,
+    if_exists="replace",  # {‘fail’, ‘replace’, ‘append’}
+)
+
 cur.execute(
     """
 SELECT

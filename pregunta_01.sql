b1a224014dc3cc1b799adf28fa3f477df7920331
jdbuilesm <114514412+jdbuilesm@users.noreply.github.com>
2023-03-07 08:30:28 -0500

    Update pregunta_01.sql

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -1,24 +1,4 @@
-tbl1 = pd.read_csv(
-    "tbl1.csv",
-    sep=",",
-    thousands=None,
-    decimal=".",
-    encoding="latin-1",
-    names=['K0', 'K1', 'c12', 'c13', 'c14', 'c15', 'c16']
-)
-
-tbl1.columns = [col.replace('-', '_') for col in tbl1.columns]
-
-tbl1.to_sql(
-    name="tbl1",
-    con=conn,
-    if_exists="replace",  # {‘fail’, ‘replace’, ‘append’}
-)
-cur.execute(
-    """
-SELECT
-    sum(c12)
-FROM
-    tbl1
-"""
-).fetchall()
+for row in cur.execute('SELECT SUM(c12) FROM tbl1'):
+    print(row)
+# Close connection to SQLite database
+conn.close()

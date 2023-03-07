1401c2a532fb09dc474fd493f6dd6864e08bded4
jdbuilesm <114514412+jdbuilesm@users.noreply.github.com>
2023-03-07 08:52:17 -0500

    Update pregunta_01.sql

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -1,1 +1,19 @@
-cur.execute('SELECT sum(c12) FROM tbl1')
+-- 
+--  La tabla `tbl1` tiene la siguiente estructura:
+-- 
+--    K0  CHAR(1)
+--    K1  INT
+--    c12 FLOAT
+--    c13 INT
+--    c14 DATE
+--    c15 FLOAT
+--    c16 CHAR(4)
+--
+--  Escriba una consulta en SQL que devuelva la suma del campo c12.
+-- 
+--  Rta/
+--     SUM(c12)
+--  0  15137.63
+--
+--  >>> query = cur.execute('SELECT sum(c12) FROM tbl1') <<<
+--

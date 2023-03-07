829e01a5b1e55ad8d2068cb3b01150172fa5b6b8
jdbuilesm <114514412+jdbuilesm@users.noreply.github.com>
2023-03-07 09:40:08 -0500

    Update pregunta_11.sql

--- pregunta_11.sql
+++ pregunta_11.sql
@@ -38,13 +38,4 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 -- 
-SELECT
-    strftime('%Y', c14) AS "YEAR",
-    COUNT(*) AS "CANT"
-
-
-FROM
-    tbl1
-
-WHERE
-    YEAR == "2018";
+SELECT COUNT(*) FROM tbl1 WHERE c14(date) = 2018
